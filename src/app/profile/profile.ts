24aa7413ca0dc13269c90d8fac990386b296ef85
muz1058 <muzaffarali333gmail.com>
2025-09-12 11:30:19 +0500

    calling Api feom json server

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,67 +1,86 @@
-import { Component } from "@angular/core";
-import { createLinkedSignal } from "@angular/core/primitives/signals";
-import { ActivatedRoute } from "@angular/router";
-import { Albums } from "../services/albums";
-import { CommonModule } from "@angular/common";
-
+import { AfterContentInit, ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
+import { createLinkedSignal } from '@angular/core/primitives/signals';
+import { ActivatedRoute } from '@angular/router';
+import { Albums } from '../services/albums';
+import { CommonModule } from '@angular/common';
+import { Users } from '../services/users';
 
 @Component({
-    selector: 'profile',
-    imports: [CommonModule],
-    // template:'<p>i am profile</p>',
-    // styles:'p{background-color:red}'
-    templateUrl: './profile.html',
-    styleUrl: './profile.css'
-
+  selector: 'profile',
+  imports: [CommonModule],
+  // template:'<p>i am profile</p>',
+  // styles:'p{background-color:red}'
+  templateUrl: './profile.html',
+  styleUrl: './profile.css',
 })
+export class ProfileComponent implements OnInit {
+  isUserLoaded: boolean = false;
+  isAlbumLoaded: boolean = false;
 
-export class ProfileComponent {
+  // handleEvent(event:Event){
+  //     console.log("VAlue ::",(event.target as HTMLInputElement).value)
+  //     console.log("Type :: ",event.type)
+  // }
 
-    // handleEvent(event:Event){
-    //     console.log("VAlue ::",(event.target as HTMLInputElement).value)
-    //     console.log("Type :: ",event.type)
-    // }
+  // name="";
+  // displayName=""
 
-    // name="";
-    // displayName=""
+  // getName(event:Event){
+  //     this.name=(event.target as HTMLInputElement).value
+  //     console.log("Event ",this.name)
+  // }
 
-    // getName(event:Event){
-    //     this.name=(event.target as HTMLInputElement).value
-    //     console.log("Event ",this.name)
-    // }
+  // showName(){
+  //     this.displayName=this.name
+  // }
 
-    // showName(){
-    //     this.displayName=this.name
-    // }
+  userName: string | null = '';
+  userId: string | null = '';
 
-    userName: string | null = ""
-    userId: string | null = ""
+  constructor(
+    private route: ActivatedRoute,
+    private albumService: Albums,
+    private userService: Users,
+    private cd : ChangeDetectorRef
+  ) {
+    // passing data via routerLink
 
-    constructor(private route: ActivatedRoute, private albumService: Albums) {
-        // passing data via routerLink
+    // let data=this.route.snapshot.paramMap.getAll
+    // console.log("Data from pramaAll :: ",data)
+    //  this.userName=this.route.snapshot.paramMap.get('name');
+    //  this.userId=this.route.snapshot.paramMap.get('id')
 
-        // let data=this.route.snapshot.paramMap.getAll
-        // console.log("Data from pramaAll :: ",data)
-        //  this.userName=this.route.snapshot.paramMap.get('name');
-        //  this.userId=this.route.snapshot.paramMap.get('id')
+    // getting data via button click from Home page
 
-        // getting data via button click from Home page
+    this.route.queryParams.subscribe((params) => {
+      this.userName = params['name'];
+      this.userId = params['id'];
+    });
+  }
+  albums: any = [];
+  users: any = [];
+  ngOnInit() {
+    debugger;
+    this.loadAlbum();
+    this.loadUsers();
+  }
 
-        this.route.queryParams.subscribe(params => {
-            this.userName = params['name']
-            this.userId = params['id']
-        })
+  loadUsers() {
+    this.userService.getUsers().subscribe((data) => {
+      
+               this.users = data;
+               this.isUserLoaded = true;
+       
+      console.log('Users Data: ', data);
+    });
+  }
 
-
-
-
-    }
-    albums: any=[];
-    ngOnInit() {
-        this.albumService.getAlbums().subscribe(data=>{
-            this.albums=data;  
-            console.log(this.albums);                      
-        })
-    }
-
+  loadAlbum() {
+    this.albumService.getAlbums().subscribe((data) => {
+      this.albums = data;
+      this.isAlbumLoaded = true;
+      console.log(this.albums);
+      this.cd.markForCheck();
+    });
+  }
 }
---
+++ src/app/services/users.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Users {
+   users:any;
+  constructor(private http:HttpClient){
+
+  }
+ 
+  getUsers(){
+    const url="http://localhost:3000/users"
+     return this.http.get(url)
+    }
+  
+}
